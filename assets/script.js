38f9d19ea7c82f21558a955d3532fef21201ef83
João Pedro Santos Pereira <jps.pereira0805@gmail.com>
2024-02-04 14:53:58 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -9,13 +9,34 @@ class User {
     }
 
     validateForm() {
-        this.blank();
+        (this.blank()) ;
+        (this.checkNameAndSurname()) ;
         this.checkCPF();
         this.checkUser();
         this.checkPassword();
 
     }
 
+    
+    
+    innerErrorMessage(element, mensagem) {
+        const existingSpan = element.querySelector('.error-message');
+        
+        if (!existingSpan) {
+            const newSpan = document.createElement('span');
+            newSpan.textContent = '*' + mensagem;
+            newSpan.className = 'error-message';
+            element.appendChild(newSpan);
+        } 
+    }
+    
+    removeErrorMessage(element) {
+        const existingSpan = element.querySelector('.error-message');
+        if (existingSpan) {
+            existingSpan.remove();
+        }
+    }
+    
     blank() {
         const array = [this.name, this.surname, this.cpf, this.user, this.password, this.repeatPassword];
         
@@ -24,51 +45,58 @@ class User {
             const element = document.getElementById(`${i}-element`);
             
             if (obj === '') {
-                
-                const existingSpan = element.querySelector('.error-message');
-                
-                if (!existingSpan) {
-                    const newSpan = document.createElement('span');
-                    newSpan.innerText = 'Este campo não pode estar em branco';
-                    newSpan.className = 'error-message';
-                    element.appendChild(newSpan);
-                }
-            }  else {
-                const existingSpan = element.querySelector('.error-message');
-                if (existingSpan) {
-                    existingSpan.remove();
-                }
+                this.innerErrorMessage(element, 'Esse campo não pode estar em branco');   
+            } else {
+                this.removeErrorMessage(element);
             }
         }
-        return;
+    }
+    
+    checkNameAndSurname() {
+        const nameContainer = document.getElementById('0-element');
+        if (/[^a-zA-Z\u00C0-\u017F\s]/.test(this.name) || this.name.trim() === '') {
+            const nameContainer = document.getElementById('0-element');
+            this.innerErrorMessage(nameContainer, 'Nome inválido');
+        } 
+
+        const surnameContainer = document.getElementById('1-element');
+        if (/[^a-zA-Z\u00C0-\u017F\s]/.test(this.surname) || this.surname.trim() === '') {
+            this.innerErrorMessage(surnameContainer, 'Sobrenome inválido');
+        }
     }
 
     checkCPF() {
         const cpf = new Cpf(this.cpf);
+        const cpfContainer = document.getElementById('2-element');
         
         if(!(cpf.validateCpf())) {
-            console.log('CPF inválido');
+            this.innerErrorMessage(cpfContainer, 'CPF inválido');
         } 
     }
 
 
     checkUser() {
+        const userContainer = document.getElementById('3-element');
+
         if (/[^a-zA-Z0-9]/.test(this.user)) {
-            console.log('O usuário não pode conter caracteres especiais')
-        }
+            this.innerErrorMessage(userContainer, 'O usuário não pode conter caracteres especiais');
+        } 
+
         if (this.user.length < 3 || this.user.length > 12) {
-            console.log('O usuário deve ter entre 3 e 12 caracteres');
-        }
-        return;
+            this.innerErrorMessage(userContainer, 'O usuário deve ter entre 3 e 12 caracteres');
+        } 
     }
 
     checkPassword() {
+        const passwordContainer = document.getElementById('4-element');
+        const repeatPasswordContainer = document.getElementById('5-element');
+        
         if (this.password.length < 6 || this.password.length > 12) {
-            console.log('A senha deve ter entre 6 e 12 caracteres');
+            this.innerErrorMessage(passwordContainer, 'A senha deve ter entre 6 e 12 caracteres');
         }
 
         if (this.password !== this.repeatPassword) {
-            console.log('As senhas não condizem')
+            this.innerErrorMessage(repeatPasswordContainer, 'As senhas não condizem');
         }
 
         return;
